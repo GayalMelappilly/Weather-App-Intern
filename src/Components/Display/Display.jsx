86bbcac73e75bc0736e50566b05d7923e55e73e9
Gayal Melappilly <gayalsunil@gmail.com>
2024-04-03 01:56:30 +0530

    Trying out Grid

--- src/Components/Display/Display.jsx
+++ src/Components/Display/Display.jsx
@@ -82,20 +82,20 @@ function Display() {
     return (
 
         <div className='mx-auto h-5/6 bg-black bg-opacity-10 justify-center flex py-10 shadow-2xl shadow-black-400 w-5/6 rounded-lg       max-sm:grid max-sm:py-3'>
-            <div className='w-5/12              max-sm:w-80 max-sm:mx-3 max-sm:h-4/6'>
+            <div className='w-5/12              max-sm:w-72 max-sm:mx-auto max-sm:h-4/6'>
                 <div className='px-10 flex items-center bg-white bg-opacity-10 h-1/6 rounded-lg shadow-lg shadow-black-400         max-sm:w-full max-sm:px-6'>
-                    <div className='py-4 relative     max-sm:mx-2 max-sm:w-full max-sm:py-6'>
+                    <div className='py-4 relative     max-sm:w-full max-sm:py-2'>
                         <input className='pl-10 pr-4 py-2 rounded-lg outfit-regular rounded-lg h-8 bg-black bg-opacity-10 placeholder-white text-white outline-none           max-sm:w-40 max-sm:h-6 ' value={input} onChange={(e) => { setInput(e.target.value) }} type="text" id='city' placeholder='Search for location' />
                         <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
                             <FontAwesomeIcon className='text-white' icon={faSearch} />
                         </div>
                         &nbsp;
-                        <button onClick={(e) => { setCity(input) }} className='px-6 bg-black absolute bg-opacity-10 rounded-lg h-8 text-white         max-sm:h-6'>Search</button>
+                        <button onClick={(e) => { setCity(input) }} className='px-6 bg-black absolute bg-opacity-10 rounded-lg h-8 text-white       max-sm:px-5  max-sm:h-6'>Search</button>
                     </div>
                 </div>
                 <div className="py-2 max-sm:py-1"></div>
                 <div className={`bg-white bg-opacity-10 rounded-lg shadow-lg shadow-black-400 h-2/6       max-sm:h-16`}>
-                    {details && <div className='flex justify-start      max-sm:my-auto'>
+                    {details && <div className='flex justify-start     '>
                         <Icon icon={details.weather[0].icon} />
                         <div className='pt-10 pl-3    max-sm:pt-1 max-sm:inline-block max-sm:align-middle'>
                             <p className='font-thin text-3xl max-sm:text-base'>{details.main.temp}°C</p>
@@ -110,38 +110,38 @@ function Display() {
                 <div className="py-2 max-sm:py-1"></div>
                 
                 <div>
-                { details ? <Forecast /> : <div className='bg-white w-full bg-opacity-10 h-56 rounded-lg shadow-lg shadow-black-400'></div> } 
+                { details ? <Forecast /> : <div className='bg-white w-full bg-opacity-10 h-44 rounded-lg shadow-lg shadow-black-400         max-sm:h-20 max-sm:p-2 max-sm-pb-2'></div> } 
                 </div>
             </div>
 
             <div className='px-2'></div>
 
-            <div className="w-6/12      max-sm:w-80 max-sm:mx-3 max-sm:h-4/6">
+            <div className="w-6/12      max-sm:w-72 max-sm:mx-auto max-sm:h-4/6">
                 <div>
                     {details ? <div className='h-full text-xl font-thin flex justify-between items-center        max-sm:w-full max-sm:px-1 max-sm:text-xs'>
-                        <div className='px-3 py-4 bg-white w-full bg-opacity-10 h-24 rounded-lg shadow-lg shadow-black-400      max-sm:px-2 max-sm:h-20'>
+                        <div className='px-3 py-4 bg-white w-full bg-opacity-10 h-24 rounded-lg shadow-lg shadow-black-400      max-sm:px-1.5 max-sm:h-20'>
                             <BsWind size={30} className='mx-auto fill-slate-600' />
                             <p className='text-center'>{details.wind.speed}km/h</p>
                         </div>
-                        <div className='px-3 py-4 mx-2 bg-white w-full bg-opacity-10 h-24 rounded-lg shadow-lg shadow-black-400      max-sm:px-2 max-sm:h-20'>
+                        <div className='px-3 py-4 mx-2 bg-white w-full bg-opacity-10 h-24 rounded-lg shadow-lg shadow-black-400      max-sm:px-1.5 max-sm:h-20'>
                             <BsDroplet size={30} className='mx-auto fill-slate-600' />
                             <p className='text-center'>{details.main.humidity}%</p>
                         </div>
-                        <div className='px-3 py-4 mr-2 bg-white w-full bg-opacity-10 h-24 rounded-lg shadow-lg shadow-black-400      max-sm:px-2 max-sm:h-20'>
+                        <div className='px-3 py-4 mr-2 bg-white w-full bg-opacity-10 h-24 rounded-lg shadow-lg shadow-black-400      max-sm:px-1.5 max-sm:h-20'>
                             <BsThermometer size={30} className='mx-auto fill-slate-600' />
                             <p className='text-center'>{details.main.temp}°C</p>
                         </div>
-                        <div className='px-3 py-4 mr-2 bg-white w-full bg-opacity-10 h-24 rounded-lg shadow-lg shadow-black-400      max-sm:px-2 max-sm:h-20'>
+                        <div className='px-3 py-4 mr-2 bg-white w-full bg-opacity-10 h-24 rounded-lg shadow-lg shadow-black-400      max-sm:px-1.5 max-sm:h-20'>
                             <SlSpeedometer size={30} className='mx-auto fill-slate-600' />
                             <p className='text-center'>{details.main.pressure} hPa</p>
                         </div>
-                        <div className='px-3 py-4 bg-white w-full bg-opacity-10 h-24 rounded-lg shadow-lg shadow-black-400      max-sm:px-2 max-sm:h-20'>
+                        <div className='px-3 py-4 bg-white w-full bg-opacity-10 h-24 rounded-lg shadow-lg shadow-black-400      max-sm:px-1.5 max-sm:h-20'>
                             <BsClouds size={30} className='mx-auto fill-slate-600' />
                             <p className='text-center'>{details.clouds.all}</p>
                         </div>
                     </div>
                         :
-                        <div className='bg-white w-full bg-opacity-10 h-24 rounded-lg shadow-lg shadow-black-400'>
+                        <div className='bg-white w-full bg-opacity-10 h-24 rounded-lg shadow-lg shadow-black-400         max-sm:w-full'>
                         </div>}
                 </div>
                 <div className="py-2 max-sm:py-1"></div>
