26add01ef920275b318e51767b288b85a6bd032e
Gayal Melappilly <gayalsunil@gmail.com>
2024-04-03 23:37:41 +0530

    Header done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     <div className='app w-full bg-gradient-to-br from-cyan-300 to-gray-50'>
       <div>
       {/* <div className='bg-design relative'></div> */}
+      <div className='h-4'></div>
         <Header></Header>
         <div className='h-auto'>
           <Display></Display>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,8 +3,8 @@ import './Header.css'
 
 function Header() {
   return (
-    <div className='flex py-8 px-8 justify-start'>
-      <h1 className="outfit-bold text-3xl text-white">WEATHER APP</h1>
+    <div className='flex mx-auto my-5 justify-center bg-black bg-opacity-10 py-8 shadow-2xl shadow-black-400 w-5/6 rounded-lg  max-sm:py-2'>
+      <h1 className="outfit-bold text-6xl text-white geo-sans text-black      max-sm:text-3xl">WEATHER AND FORCAST APP</h1>
     </div>
   )
 }
