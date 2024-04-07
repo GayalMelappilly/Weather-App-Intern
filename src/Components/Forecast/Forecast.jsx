1b341dd8e59ba0598bc686fbb92fd880aa0e326b
Gayal Melappilly <gayalsunil@gmail.com>
2024-04-07 17:37:06 +0530

    Fixed: Forcast card issue

--- src/Components/Forecast/Forecast.jsx
+++ src/Components/Forecast/Forecast.jsx
@@ -195,11 +195,11 @@ function Forecast() {
                   setHourlyForecast(hourly[index]);
                 }}
               >
-                <div className='flex h-24    max-lg:h-20 max-sm:grid'>
+                <div className='flex h-24    max-lg:h-20  max-sm:h-12 '>
                   <Icon for='forecast' icon={obj?.weather[0].icon} />
                   <p key={index} className='pl-1 pt-4 font-normal text-lg dark:text-white    max-lg:text-xs    max-sm:pt-0 max-sm:text-xs max-sm:flex max-sm:font-light'>{obj?.main.temp_min.toFixed(0)}° / {obj?.main.temp_max.toFixed(0)}°</p>
                 </div>
-                <p key={index} className='text-base font-thin dark:text-white    max-sm:text-xs max-sm:font-light'>{toDate(obj?.dt_txt.slice(0, 14))}</p>
+                <p key={index} className='text-base font-thin dark:text-white    max-sm:text-xs max-sm:mt-3 max-sm:font-light'>{toDate(obj?.dt_txt.slice(0, 14))}</p>
               </div>
             )
         }
